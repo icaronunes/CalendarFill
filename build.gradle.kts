a14b8e28466e8176fb7fd48f0f391db6b4a15e68
Icaro <icarornunes@gmail.com>
2022-03-08 18:56:52 -0300

    changed gradle to kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("com.android.tools.build:gradle")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.39.1")
     }
 }
 plugins {
--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,21 +1,28 @@
+
+
 plugins {
-    id("com.android.application")
+    id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
     id("dagger.hilt.android.plugin")
+
+}
+
+buildscript {
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.39.1")
+    }
 }
 
 android {
     compileSdk = 31
 
     defaultConfig {
-        applicationId = "br.com.mirespeiti.calendarfill"
         minSdk = 21
         targetSdk = 31
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -34,30 +41,21 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildFeatures {
         viewBinding = true
     }
 
-    packagingOptions {
-        resources.excludes.apply {
-            add("META-INF/gradle/incremental.annotation.processors")
-        }
-    }
 }
 
 dependencies {
 
-    implementation(project(":library"))
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("com.google.android.material:material:1.6.0-alpha03")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.4.1")
-    implementation("androidx.navigation:navigation-ui-ktx:2.4.1")
-    implementation("androidx.core:core-splashscreen:1.0.0-beta01")
-    implementation("com.google.dagger:hilt-android:2.39.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.39.1")
+    implementation("com.google.android.material:material:1.5.0")
     testImplementation("junit:junit:4.13.2")
+    implementation("com.google.dagger:hilt-android:2.39.1")
+    implementation( "com.google.dagger:hilt-android-compiler:2.39.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
